134a46f3e0c7f2cb32aad38eee8ff73f3c6c1a59
KingsleyHng <125108040+KingsleyHng@users.noreply.github.com>
2024-06-10 08:47:29 +0800

    Update FIFO_tb.sv

--- FIFO_tb.v
+++ FIFO_tb.v
@@ -1,50 +1,115 @@
 `timescale 1ns/1ps
 
+module async_fifo_tb;
 
-`include "FIFO.v"
+  parameter DATA_WIDTH = 8;
+  parameter ADDR_WIDTH = 3;
 
-module FIFO_tb;
-  reg wclk;
-  reg reset;
-  reg [DATA_WIDTH-1:0] Din;
-  wire [DATA_WIDTH-1:0] Dout;
-  wire fifo_empty;
-  wire rclk;
-  wire [PTR_SIZE-1:0]wptr;
-  wire [PTR_SIZE-1:0]rptr;
-  wire wr_first_edge;
-  wire rd_first_edge;
-  wire rd_en;
+  reg write_enable, write_clk, write_reset_n;
+  reg read_enable, read_clk, read_reset_n;
+  reg [DATA_WIDTH-1:0] write_data;
+  wire [DATA_WIDTH-1:0] read_data;
+  wire fifo_full, fifo_empty;
 
-parameter DATA_WIDTH = 8;
-parameter DEPTH = 8;
-parameter PTR_SIZE = 3;
-  integer i;
+  // Model a queue for checking data
+  reg [DATA_WIDTH-1:0] verification_data_queue[$];
+  reg [DATA_WIDTH-1:0] verification_write_data;
+  reg all_test_passed = 1;
 
-  // Instantiate the FIFO module
-  FIFO uut (.wclk(wclk), .reset(reset), .rclk(rclk), .wptr(wptr), .rptr(rptr), .wr_first_edge(wr_first_edge), .rd_first_edge(rd_first_edge), 
-  .rd_en(rd_en), .Din(Din), .Dout(Dout), .fifo_empty(fifo_empty));
+  // Instantiate the FIFO
+  AsynchronousFIFO #(DATA_WIDTH, ADDR_WIDTH) dut (
+    .write_enable(write_enable), 
+    .write_clk(write_clk), 
+    .write_reset_n(write_reset_n),
+    .read_enable(read_enable), 
+    .read_clk(read_clk), 
+    .read_reset_n(read_reset_n),
+    .write_data(write_data), 
+    .read_data(read_data), 
+    .fifo_full(fifo_full), 
+    .fifo_empty(fifo_empty)
+  );
 
-  
+  // Clock generation
   initial begin
-    wclk = 0;       // Initialize the clock to 0
-    forever #5 wclk = ~wclk; // Toggle the clock every 5 time units
+    write_clk = 1'b0;
+    read_clk = 1'b0;
+
+    fork
+      forever #20 write_clk = ~write_clk;
+      forever #80 read_clk = ~read_clk;
+    join
   end
 
+  // Write operations
   initial begin
-    $dumpfile("FIFO.vcd");
-    $dumpvars();
-    reset <= 1'b1;
-    #10;
-    reset <= 1'b0;
+    write_enable = 1'b0;
+    write_data = '0;
+    write_reset_n = 1'b0;
+    repeat(5) @(posedge write_clk);
+    write_reset_n = 1'b1;
+    $display("Write domain reset deasserted");
 
-    for (i = 0; i <= 10; i = i + 1) begin
-      // Create an array to hold random values for Din
-      Din = $random;
-      #10;
+    for (int iter = 0; iter < 2; iter++) begin
+      for (int i = 0; i < 16; i++) begin
+        @(posedge write_clk && !fifo_full);
+        write_enable = (i % 2 == 0) ? 1'b1 : 1'b0;
+        if (write_enable) begin
+          write_data = $urandom;
+          verification_data_queue.push_back(write_data);
+        end
+      end
+      #1us;
+    end
+  end
+
+  // Read operations
+  initial begin
+    read_enable = 1'b0;
+    read_reset_n = 1'b0;
+    repeat(5) @(posedge read_clk);
+    read_reset_n = 1'b1;
+
+    for (int iter = 0; iter < 2; iter++) begin
+      for (int i = 0; i < 16; i++) begin
+        @(posedge read_clk && !fifo_empty);
+        read_enable = (i % 2 == 0) ? 1'b1 : 1'b0;
+        if (read_enable) begin
+          verification_write_data = verification_data_queue.pop_front();
+          // Check the read_data against modeled write_data
+          if(read_data != verification_write_data) begin
+            $error("Comparison Failed: expected write_data = %h, read_data = %h", verification_write_data, read_data);
+            all_test_passed = 0;
+          end
+          else begin
+            $display("Comparison Passed: expected write_data = %h, read_data = %h", verification_write_data, read_data);
+          end
+        end
+      end
+      #1us;
     end
 
-    // Finish the simulation after some time
+   
+  end
+  
+  initial begin
+    #10000
+    if (!all_test_passed) begin
+      $display("Test Failed");
+      $finish;
+    end
+    
+    else begin
+      $display("TEST PASSED");
+   
+    end
     $finish;
   end
+  
+  // VCD generation
+  initial begin
+    $dumpfile("async_fifo_tb.vcd");  // Name of the VCD file
+    $dumpvars(0, async_fifo_tb);     // Dump all variables in this module
+  end
+
 endmodule
